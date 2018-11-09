83271c49b7b928c9cfbabfb92414648dbc5c7349
mat@blocknot.es <mat@blocknot.es>
2018-11-09 08:32:35 +0100

    v0.2.0 - Improved editors init, improved editors styles, updated Medium Editor to 5.23.3

--- app/assets/javascripts/activeadmin/medium_editor_input.js
+++ app/assets/javascripts/activeadmin/medium_editor_input.js
@@ -1,7 +1,18 @@
-$(document).ready( function() {
+function initMediumEditors() {
   $('.medium-editor').each(function () {
-    var options = {};
-    options = $.extend({}, options, $(this).data('options'));
-    new MediumEditor( $(this), options );
+    if (!$(this).hasClass('medium-editor--active')) {
+      var options = {};
+      options = $.extend({}, options, $(this).data('options'));
+      new MediumEditor($(this), options);
+      $(this).addClass('medium-editor--active');
+    }
   });
+}
+
+$(document).on('has_many_add:after', function () {
+  initMediumEditors();
+});
+
+$(document).ready( function() {
+  initMediumEditors();
 });
--- lib/activeadmin/medium_editor/version.rb
+++ lib/activeadmin/medium_editor/version.rb
@@ -1,5 +1,5 @@
 module ActiveAdmin
   module MediumEditor
-    VERSION = '0.1.4'
+    VERSION = '0.2.0'
   end
 end
