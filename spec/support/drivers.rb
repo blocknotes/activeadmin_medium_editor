d1b0140dacc7efbafbb724d782c9352d0ddd64e2
Mattia Roccoberton <mat@blocknot.es>
2021-06-04 08:21:57 +0200

    Add linters workflow

--- spec/support/drivers.rb
+++ spec/support/drivers.rb
@@ -2,7 +2,7 @@
 
 RSpec.configure do |config|
   config.before(:each, type: :system) do
-    driver = ENV.fetch('SPEC_JS_DRIVER', :selenium_chrome_headless).to_sym
-    driven_by(driver)
+    driver = ENV.fetch('SPEC_JS_DRIVER') { :selenium_chrome_headless } # rubocop:disable Style/RedundantFetchBlock
+    driven_by(driver.to_sym)
   end
 end
